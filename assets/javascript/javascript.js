00eba804bf89dde2a015b4d011480dcde585cf8e
Benjamin Brier <ben.brier14@gmail.com>
2019-09-17 12:45:03 -0400

    created function to display gifs, problem finding data

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,35 @@
-console.log("test")
+//Create API Key variable to avoid typing the entire number
+var apikey = "Gk7GE3hsWMc3uSKfYG0G6GF3VEn8rZIS"
+
+//Create on-click event to find the value of the name the user entered and make an AJAX call
+$('#find-fighter').on("click", function(event) {
+    event.preventDefault();
+    console.log("test");
+
+    var fighter = $('#fighter-input').val();
+    var queryurl = "https://api.giphy.com/v1/gifs/search?q=" + fighter + "&api_key=" + apikey;
+
+    $.ajax({
+        url: queryurl,
+        method: 'GET'
+    })
+    .then(function(response) {
+        console.log(queryurl);
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++);
+            var fighterDiv = $('<div>');
+            var p = $('<p>').text("Rating: " + results[i].rating);
+            var fighterImage = $('<img>');
+            fighterImage.attr("src", results[i].images.fixed_height.url); 
+            fighterDiv.append(p);
+            fighterDiv.append(fighterImage);
+            $('#fighter-view').append(fighterDiv);
+    })
+
+});
+
+
+
